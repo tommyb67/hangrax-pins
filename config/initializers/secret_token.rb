2fa80e16ec804c4d68f650a8395818c4c4f808ad
Tom Brennan <tommybrennan@gmail.com>
2015-01-01 23:47:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HangraxPins::Application.config.secret_key_base = '69b06a1c77961b9e64c34cb3fa4f1975fe5dcb18bc8bd7c031f3a14c1ab341d9526ae6614359dcc80dfe5e6db5746cbd64e61acbefc0b98bce127096be060464'
